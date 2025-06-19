dc4bf0c10284df849a0f0bea72a2c08416c7f101
Francesco Altiero <francesco.altiero@unina.it>
2025-06-19 14:56:24 +0200

    Updated examples and added tests for MaSIF containers.

--- containers/masif/resources/prepare_one.sh
+++ containers/masif/resources/prepare_one.sh
@@ -1,2 +1,22 @@
-PARAMS_FILE='/masif/data/da4vid/custom_params.py'
+#!/bin/bash
+masif_root=/masif
+masif_source=$masif_root/source/
+masif_matlab=$masif_root/source/matlab_libs/
+export PYTHONPATH=$PYTHONPATH:$masif_source
+export masif_matlab
+if [ "$1" == "--file" ]
+then
+	echo "Running masif site on $2"
+	PPI_PAIR_ID=$3
+	PDB_ID=$(echo "${PPI_PAIR_ID}"| cut -d"_" -f1)
+	CHAIN1=$(echo "${PPI_PAIR_ID}"| cut -d"_" -f2)
+	FILENAME=$2
+	mkdir -p data_preparation/00-raw_pdbs/
+	cp "$FILENAME" "data_preparation/00-raw_pdbs/${PDB_ID}.pdb"
+else
+  echo "You need to specify one input file with --file option"
+  exit 255
+fi
 
+python -W ignore $masif_source/data_preparation/01-pdb_extract_and_triangulate.py "${PDB_ID}_${CHAIN1}"
+python $masif_source/data_preparation/04-masif_precompute.py masif_site "${PPI_PAIR_ID}"
